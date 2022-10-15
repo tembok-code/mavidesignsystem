89b06f87aa111cfd7d37490dd0b7365fc896611a
tembok-code <tembokpma@gmail.com>
2022-10-15 01:10:09 -0500

    updated Color Schemes content grid

--- lib/design_system_preview/content/colors.dart
+++ lib/design_system_preview/content/colors.dart
@@ -39,13 +39,28 @@ class ContentColors extends StatelessWidget {
                   maviThemeVerticalSpacer(size: spacing_lg),
                   Expanded(
                     flex: 2,
-                    child: Column(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        MaviTheme().textTheme.h4(text: 'Light'),
-                        _ColorThemeGrid(scheme: MaviTheme().light),
-                        maviThemeSpacer(size: spacing_lg),
-                        MaviTheme().textTheme.h4(text: 'Dark'),
-                        _ColorThemeGrid(scheme: MaviTheme().dark)
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              MaviTheme().textTheme.h4(text: 'Light'),
+                              _ColorThemeGrid(scheme: MaviTheme().light),
+                            ],
+                          ),
+                        ),
+                        maviThemeVerticalSpacer(size: spacing_lg),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              MaviTheme().textTheme.h4(text: 'Dark'),
+                              _ColorThemeGrid(scheme: MaviTheme().dark),
+                            ],
+                          ),
+                        )
                       ],
                     ),
                   ),
@@ -56,7 +71,10 @@ class ContentColors extends StatelessWidget {
         ]),
         SectionWidget(title: "$title - Swatches", slivers: [
           SliverToBoxAdapter(
-            child: MaviTheme().textTheme.h4(text: 'Primary'),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: margin_lg),
+              child: MaviTheme().textTheme.h4(text: 'Primary'),
+            ),
           ),
           SliverToBoxAdapter(
             child: Padding(
@@ -76,94 +94,82 @@ class _ColorThemeGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double height_lg = 72;
-    double height_sm = 24;
+    const double height_lg = spacing_lg * 2;
+    const double height_sm = spacing_lg;
+
+    Widget _colorContainer(Color primary, Color onPrimary,
+            {double height = height_sm}) =>
+        Container(
+          color: primary,
+          height: height,
+          padding: const EdgeInsets.all(spacing_sm),
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              color: onPrimary,
+              height: height * 0.5,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(radius_content)),
+            ),
+          ),
+        );
     return ClipRRect(
-      borderRadius: BorderRadius.circular(height_sm),
+      borderRadius: BorderRadius.circular(radius_container),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              scheme.primary,
-              scheme.onPrimary,
-              scheme.primaryContainer,
-              scheme.onPrimaryContainer
+              [scheme.primary, scheme.onPrimary],
+              [scheme.primaryContainer, scheme.onPrimaryContainer]
             ]
                 .map((e) => Expanded(
-                        child: Container(
+                        child: _colorContainer(
+                      e[0],
+                      e[1],
                       height: height_lg,
-                      color: e,
-                      child: Column(
-                        children: [Text('$e')],
-                      ),
                     )))
                 .toList(),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              scheme.secondary,
-              scheme.onSecondary,
-              scheme.secondaryContainer,
-              scheme.onSecondaryContainer
-            ]
-                .map((e) =>
-                    Expanded(child: Container(height: height_sm, color: e)))
-                .toList(),
+              [scheme.secondary, scheme.onSecondary],
+              [scheme.secondaryContainer, scheme.onSecondaryContainer]
+            ].map((e) => Expanded(child: _colorContainer(e[0], e[1]))).toList(),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              scheme.tertiary,
-              scheme.onTertiary,
-              scheme.tertiaryContainer,
-              scheme.onTertiaryContainer
-            ]
-                .map((e) =>
-                    Expanded(child: Container(height: height_sm, color: e)))
-                .toList(),
+              [scheme.tertiary, scheme.onTertiary],
+              [scheme.tertiaryContainer, scheme.onTertiaryContainer]
+            ].map((e) => Expanded(child: _colorContainer(e[0], e[1]))).toList(),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              scheme.error,
-              scheme.onError,
-              scheme.errorContainer,
-              scheme.onErrorContainer
-            ]
-                .map((e) =>
-                    Expanded(child: Container(height: height_sm, color: e)))
-                .toList(),
+              [scheme.error, scheme.onError],
+              [scheme.errorContainer, scheme.onErrorContainer]
+            ].map((e) => Expanded(child: _colorContainer(e[0], e[1]))).toList(),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              scheme.background,
-              scheme.onBackground,
-              scheme.surface,
-              scheme.onSurface
-            ]
-                .map((e) =>
-                    Expanded(child: Container(height: height_sm, color: e)))
-                .toList(),
+              [scheme.background, scheme.onBackground],
+              [scheme.surface, scheme.onSurface]
+            ].map((e) => Expanded(child: _colorContainer(e[0], e[1]))).toList(),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              scheme.outline,
-              scheme.outline,
-              scheme.surfaceVariant,
-              scheme.onSurfaceVariant,
-            ]
-                .map((e) =>
-                    Expanded(child: Container(height: height_sm, color: e)))
-                .toList(),
+              [scheme.outline, scheme.outline],
+              [scheme.surfaceVariant, scheme.onSurfaceVariant]
+            ].map((e) => Expanded(child: _colorContainer(e[0], e[1]))).toList(),
           ),
         ]
             .map((e) => Padding(
-                  padding: EdgeInsets.only(bottom: spacing_sm),
+                  padding: const EdgeInsets.only(bottom: spacing_sm),
                   child: e,
                 ))
             .toList(),
@@ -182,8 +188,9 @@ class _ColorSwatchRow extends StatelessWidget {
             width: double.infinity,
             height: button_height_lg * 2,
             color: color,
+            padding: const EdgeInsets.all(spacing_sm),
             child: Align(
-                alignment: Alignment.bottomCenter,
+                alignment: Alignment.bottomLeft,
                 child: MaviTheme().textTheme.body(
                       text:
                           '$shade\n${color.toString().replaceAll('Color(0xff', "").replaceAll(")", "")}',
